906d89cc5a3016a4cd6ee1a29ca489193439f4e2
sukant43 <sukant98657@gmail.com>
2025-05-25 10:39:29 +0530

    otp sent successfully

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
-const mysql=require('mysql2/promise');
+const mongoose = require('mongoose');
 
-const pool=mysql.createPool({
-    host: process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port:process.env.DB_PORT,
-});
+const connectDb=()=>{
+    try{
+        mongoose.connect(process.env.MONGO_URI);
+        console.log('Database connection successful');
+    }
+    catch(error){
+        console.error('Database connection failed:', error);
+        process.exit(1); 
+    }
+}
 
-module.exports={pool};
+module.exports = connectDb;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,34 @@
 const express=require('express');
 const env=require('dotenv').config();
 const app=express();
-const {pool}=require('./config/db')
+const connectDb=require('./config/db')
 const cors=require('cors');
+const transporter=require('./config/email');
 app.use(cors());
 app.use(express.json());
 
+app.post('/send-test-email', async (req, res) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,          
+      to: 'sukantc005@gmail.com',             
+      subject: 'Test Email from Node.js App',
+      text: 'Hello! This is a test email sent from the Express app using Nodemailer.',
+    });
 
+    console.log('Message sent: %s', info.messageId);
+    res.status(200).json({ message: 'Test email sent successfully', messageId: info.messageId });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ error: 'Failed to send test email' });
+  }
+});
 
 
 
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
-    if(pool){
-        console.log('Database connection successful');
-    }else{
-        console.log('Database connection failed');
-    }
+    connectDb();
 }
 );
