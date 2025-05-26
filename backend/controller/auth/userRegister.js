7b5167425df499d14af23663a4381862be909fe8
sukant43 <sukant98657@gmail.com>
2025-05-26 17:50:55 +0530

    user register success

--- backend/controller/auth/userRegister.js
+++ backend/controller/auth/userRegister.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcryptjs');
 
 const createOtp = async (req, res) => {
     const { email } = req.body;
-
     if (!email || typeof email !== 'string') {
         return res.status(400).json({ message: 'Invalid email format' });
     }
